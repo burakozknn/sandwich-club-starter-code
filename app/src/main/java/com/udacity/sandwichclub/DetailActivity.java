eb6c95ca737c7b8ade6e8b6f5dc5fbb13e1ca20a
burakozknn <burakozknn@gmail.com>
2018-05-22 02:12:14 +0300

    updated

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,10 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +18,15 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    TextView also_known_tv_label,also_known_tv,ingredients_tv,origin_tv,description_tv;
+    ImageView ingredientsIv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +62,50 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        // get alsoKnownAs
+
+        also_known_tv_label = (TextView) findViewById( R.id.also_known_tv_label );
+        also_known_tv = (TextView) findViewById( R.id.also_known_tv );
+
+        if (sandwich.getAlsoKnownAs() !=null && !sandwich.getAlsoKnownAs().isEmpty()){
+            for ( int i =0; i<sandwich.getAlsoKnownAs().size(); i++) {
+                also_known_tv.append("- " + sandwich.getAlsoKnownAs().get(i) + "\n" );
+            }
+        } else {
+            also_known_tv.setVisibility( View.GONE );
+            also_known_tv_label.setVisibility( View.GONE );
+        }
+
+        // get ingredients
+
+        ingredients_tv = (TextView) findViewById( R.id.ingredients_tv );
+        if (sandwich.getIngredients() !=null && !sandwich.getIngredients().isEmpty()){
+            for ( int i =0; i<sandwich.getIngredients().size(); i++) {
+                ingredients_tv.append( "- " + sandwich.getIngredients().get(i) + "\n"  );
+            }
+        } else {
+            ingredients_tv.setVisibility( View.GONE );
+
+        }
+
+        // get description
+
+        description_tv = (TextView) findViewById( R.id.description_tv );
+        description_tv.setText( sandwich.getDescription());
+
+        // get placeOfOrigin
+
+        origin_tv = (TextView) findViewById( R.id.origin_tv );
+
+        if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().isEmpty()) {
+            origin_tv.setText( sandwich.getPlaceOfOrigin());
+        } else {
+            origin_tv.setText( "unknown" );
+        }
+
+
 
     }
 }
